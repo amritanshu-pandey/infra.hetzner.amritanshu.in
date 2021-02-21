00760513628fa4fa836451e5a68c577ffb02e8eb
Amritanshu Pandey <email@amritanshu.in>
2021-02-22 10:17:35 +1100

    add support for orchestration using ansible

--- terrraform/servers.tf
+++ terrraform/servers.tf
@@ -1,3 +1,5 @@
+variable "duckdns_token" {}
+
 resource "hcloud_network" "network" {
   name     = "network"
   ip_range = "10.15.0.0/16"
@@ -10,8 +12,9 @@ resource "hcloud_network_subnet" "amritanshu_in_network_subnet" {
   ip_range     = "10.15.1.0/24"
 }
 
-resource "hcloud_server" "kubenode1" {
-  name        = "kubenode1"
+resource "hcloud_server" "kubenode" {
+  count       = 3 
+  name        = "kubenode${count.index+1}"
   server_type = "cx11"
   image       = "ubuntu-20.04"
   location    = "nbg1"
@@ -19,22 +22,61 @@ resource "hcloud_server" "kubenode1" {
   user_data = <<-EOF
 		#! /bin/bash
     sudo apt-get update
-		sudo apt install curl wget -y
-    sudo adduser serveruser -m
-    sudo mkdir /home/serveruser/.ssh
-    sudo cp /root/.ssh/authorized_keys /home/amritanshu/.ssh/
-    sudo chown -R serveruser:serveruser /home/amritanshu/.ssh
-    sudo chmod 700 /home/amritanshu/.ssh
-    sudo chmod 600 /home/amritanshu/authorized_keys
-    curl https://get.docker.com | sudo sh
-    sudo usermod -aG sudo serveruser
-    sudo usermod -aG docker serveruser
-    echo -e "\nserveruser ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
-		echo "<h1>Deployed via Terraform</h1>" | sudo tee ~/bootstrap.log
+		sudo apt install git ansible -y
+
+    mkdir /tmp/bootstrap
+    cd /tmp/bootstrap
+    git clone https://github.com/amritanshu-pandey/infra.hetzner.amritanshu.in.git .
+    cd ansible && ansible-playbook bootstrap.yml | tee ~/ansible.log
+		echo "<h1>Deployed via Terraform</h1>" | sudo -u serveruser  tee ~/bootstrap.log
 EOF
   network {
       network_id = hcloud_network.network.id
-      ip         = "10.15.1.5"
+      ip         = "10.15.1.${count.index+10}"
+  }
+
+  # **Note**: the depends_on is important when directly attaching the
+  # server to a network. Otherwise Terraform will attempt to create
+  # server and sub-network in parallel. This may result in the server
+  # creation failing randomly.
+  depends_on = [
+    hcloud_network_subnet.amritanshu_in_network_subnet
+  ]
+}
+
+# resource "hcloud_floating_ip" "lb" {
+#   type = "ipv4"
+#   home_location = "nbg1"
+# }
+
+# resource "hcloud_floating_ip_assignment" "lb" {
+#   floating_ip_id = hcloud_floating_ip.lb.id
+#   server_id = hcloud_server.lb.id
+# }
+
+resource "hcloud_server" "rancheroolb" {
+  name        = "rancheroolb"
+  server_type = "cx11"
+  image       = "ubuntu-20.04"
+  location    = "nbg1"
+  ssh_keys    = [hcloud_ssh_key.xpsgamewsl.id]
+  user_data = <<-EOF
+		#! /bin/bash
+    sudo apt-get update
+		sudo apt install git ansible -y
+ 
+    mkdir /tmp/bootstrap
+    cd /tmp/bootstrap
+    git clone https://github.com/amritanshu-pandey/infra.hetzner.amritanshu.in.git .
+    cd ansible && ansible-playbook bootstrap.yml | tee ~/ansible.log
+
+    # Update dns record for kubelb.duckdns.org
+    echo url="https://www.duckdns.org/update?domains=kubelb&token=${var.duckdns_token}&ip=" | curl -k -o /var/log/duck.log -K -
+		echo "<h1>Deployed via Terraform</h1>" | sudo -u serveruser  tee ~/bootstrap.log
+EOF
+  network {
+      network_id = hcloud_network.network.id
+      ip         = "10.15.1.1"
   }
 
   # **Note**: the depends_on is important when directly attaching the
